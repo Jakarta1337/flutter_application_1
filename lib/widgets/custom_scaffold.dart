8d4230d15deaadc9a19e0dd3a13cf7be5886b5bf
Jakarta1337 <64095345+Jakarta1337@users.noreply.github.com>
2025-04-21 09:36:03 +0200

    update

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CustomScaffold extends StatelessWidget {
   const CustomScaffold({super.key, this.child});
   final Widget? child;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
